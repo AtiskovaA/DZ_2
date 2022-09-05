c9785abb0bda58c008de27913c89c3441aa7d143
Atiskova Anastasia <mailbox.nk@mail.ru>
2022-09-05 22:30:09 +0300

    Добавили метод обрезанного среднего

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
 
-        SalesManager oSale = new SalesManager(new int []{});
+        SalesManager oSale = new SalesManager(new int[]{});
         oSale.max();
 
-
+        System.out.println(oSale.ASale());
 
 
     }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,26 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int ASale() {
+        int result = 0;
+
+        for (int sale : sales) {
+            result += sale;
+
+        }
+        return (result - max() - min()) / (sales.length - 2);
+    }
+
+
 }
