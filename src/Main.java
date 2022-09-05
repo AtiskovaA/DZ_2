0188345e6c2d393653b3b5a685f43907aafb2257
Atiskova Anastasia <mailbox.nk@mail.ru>
2022-09-05 22:13:54 +0300

    Замена int на long

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
 
-        SalesManager oSale = new SalesManager(new int []{});
+        SalesManager oSale = new SalesManager(new long[]{});
         oSale.max();
-
-
 
 
     }
